1a8143b78001891a460b73c453318bd875dc8566
Ariel Ekgren <ariel.ekgren@gmail.com>
2014-04-18 18:49:00 +0200

    Improved mapping

--- Random_Indexing/Mapping.py
+++ Random_Indexing/Mapping.py
@@ -2,15 +2,17 @@ __author__ = 'Ariel Ekgren, https://github.com/ekgren/'
 
 import numpy as np
 
+
 class Mapping(object):
     """
     Map object that maps arbitrary data to integer in the order provided
     from 0 to length of input.
     """
 
-    def __init__(self, map={}, inverse_map=None):
-        self.map = map
-        self.inverse_map = inverse_map
+    def __init__(self):
+        self.map = {}
+        self.inverse_map = None
+        self.word_space = None
 
     def create_map(self, data):
         """
@@ -18,10 +20,42 @@ class Mapping(object):
         for i, point in enumerate(data):
             self.map[point] = i
 
+        self.create_inverse_map()
+
     def create_inverse_map(self):
         """
         """
         self.inverse_map = np.zeros(len(self.map), dtype=np.dtype)
 
         for x in self.map.iteritems():
-            self.inverse_map[x[0]] = x[1]
+            self.inverse_map[x[1]] = x[0]
+
+    def map_sequence(self, sequence):
+        """
+        Yeah.
+        """
+        #print sequence
+        mapped_sequence = np.zeros(len(sequence))
+
+        for i, item in enumerate(sequence):
+            #print item
+            #print self.map[item]
+            if item in self.map:
+                #print "test"
+                mapped_sequence[i] = self.map[item]
+            else:
+                mapped_sequence[i] = np.nan
+
+        return mapped_sequence
+
+    def nearest_neighbours(self, target, n=5):
+        """
+        """
+        if self.word_space != None:
+            if target in self.map and self.map[target] < self.word_space\
+                    .size:
+                neighb_args, neighb_vals = self.word_space.nearest_neighbours(
+                    self.map[target], n)
+                return target, self.inverse_map[neighb_args]
+            else:
+                return "No target!"
